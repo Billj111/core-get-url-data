211d965a9a2e7b5ef8d20bc4187345b37f874e51
Billj111 <5055425+Billj111@users.noreply.github.com>
2020-10-19 10:06:11 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace GetDataFrom_URL
+{
+
+    class Program
+    {
+        private static readonly HttpClient _client = new HttpClient();
+
+        static async Task Main(string[] args)
+        {
+            var repositories = await _ProcessRepositories();
+
+            foreach (var repo in repositories)
+            {
+                Console.WriteLine(repo.Description);
+                Console.WriteLine(repo.GitHubHomeUrl);
+                Console.WriteLine(repo.Homepage);
+                Console.WriteLine(repo.Watchers);
+                Console.WriteLine();
+                Console.WriteLine(repo.LastPush);
+            }
+
+            //await _GetRawDataFromURL();
+        }
+
+        /// <summary>
+        /// Processes data and ignores unwanted data in the json return
+        /// </summary>
+        /// <returns></returns>
+        private static async Task<List<Repository>> _ProcessRepositories()
+        {
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            _client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var streamTask = _client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+            var repositories = await JsonSerializer.DeserializeAsync<List<Repository>>(await streamTask);
+            return repositories;
+        }
+
+
+        /// <summary>
+        /// Brings back the raw data string 
+        /// </summary>
+        /// <returns></returns>
+        private static async Task _GetRawDataFromURL()
+        {
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            _client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var stringTask = _client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+
+            var msg = await stringTask;
+            Console.Write(msg);
+        }
+    }
+}
